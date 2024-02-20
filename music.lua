d74cc82766d7d03c271b8d50d396afc8692d8aee
ItsKiggy <88223163+ItsKiggy@users.noreply.github.com>
2024-02-20 15:48:09 -0600

    Update music.lua
    Test for .mp3

--- music.lua
+++ music.lua
@@ -4,9 +4,11 @@
     Album = "Album", -- optional
     Cover = "cover.jpg" -- optional. a link to the cover image
 },
-["song_file.mp3"] = { -- The song file or a link to the song. (Youtube links does NOT work, it needs to be a direct link to a .mp3, .ogg, etc... file)
-    Title = "Song Title", -- The title of the song
-    Artist = "Artist Name", -- The name of the artist
-    Album = "Album", -- optional
-    Cover = "cover.jpg" -- optional. a link to the cover image
+
+["https://drive.google.com/file/d/1ukV2WlpQqqruRU1pYCUGgLKdJNlXn3qV/view?usp=drive_link"] = { -- The song file or a link to the song. (Youtube links does NOT work, it needs to be a direct link to a .mp3, .ogg, etc... file)
+    Title = "Smells Like Teen Spirit", -- The title of the song
+    Artist = "Nirvana", -- The name of the artist
+    Album = "Nevermind", -- optional
+    Cover = "" -- optional. a link to the cover image
+    Genre = "Rock" -- optional 
 },
